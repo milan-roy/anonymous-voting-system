69594eba9adddf7fd6463a6ed909dd978842b009
Milan Roy <milanroy.in@gmail.com>
2021-07-19 00:10:13 +0530

    Made changes to Log in and Sign up Pages

--- app.py
+++ app.py
@@ -1,20 +1,32 @@
 from flask import Flask, render_template, url_for
+from forms import logform,regform
 
 
 def create_app():
     app = Flask("webapp")
+    app.config['SECRET_KEY'] = '216d998a37a90347dfc7b1b729932202'
 
     @app.route('/')
     @app.route('/home')
     def home():
-        return render_template('base.html', _class=['nav-link active', 'nav-link ', 'nav-link '], ariacurrent=['aria-current="page"', '', ''])
+        return render_template('base.html', 
+        _class=['nav-link active', 'nav-link ', 'nav-link '], 
+        ariacurrent=['aria-current="page"', '', ''])
 
     @app.route('/login')
     def login():
-        return render_template('base.html', _class=['nav-link ', 'nav-link active', 'nav-link '], ariacurrent=[' ', 'aria-current="page"', ''])
+        form=logform()
+        return render_template('login.html', 
+        _class=['nav-link ', 'nav-link active', 'nav-link '], 
+        ariacurrent=[' ', 'aria-current="page"', ''],
+        form=form)
 
     @app.route('/signup')
     def signup():
-        return render_template('base.html', _class=['nav-link ', 'nav-link ', 'nav-link active'], ariacurrent=['', 'aria-current="page"', ''])
+        form=regform()
+        return render_template('signup.html', 
+        _class=['nav-link ', 'nav-link ', 'nav-link active'], 
+        ariacurrent=['', 'aria-current="page"', ''],
+        form=form)
 
     return app
--- forms.py
+++ forms.py
@@ -1,15 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo,Email
 
 class regform (FlaskForm):
     username= StringField('Username',validators=[DataRequired(),Length(min=3,max=15)])
+    email=StringField('Email',validators=[DataRequired(),Email()])
     password= PasswordField('Password', validators=[DataRequired(),Length(min=4)])
-    confirm_password=PasswordField('Password', validators=[DataRequired(),Length(min=4),EqualTo('password')])
+    confirm_password=PasswordField('Confirm Password', validators=[DataRequired(),Length(min=4),EqualTo('password')])
     submit= SubmitField('Sign Up')
 
 class logform (FlaskForm):
-    username= StringField('Username',validators=[DataRequired(),Length(min=3,max=15)])
+    email=StringField('Email',validators=[DataRequired(),Email()])
     password= PasswordField('Password', validators=[DataRequired(),Length(min=4)])
     remember_me=BooleanField('Remember Me')
     submit= SubmitField('Log In')
